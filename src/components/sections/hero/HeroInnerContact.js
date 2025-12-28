326a82fd0df6bada0a48c14872d3df7fad1b0778
Akash Ramani <akashr@codemechsolutions.com>
2025-12-28 23:01:55 +0530

    Enhanced `Contact3Mod`, `HeroInnerContact`, and footer components: adjusted layout spacing with `mt-minus-350`, added fun facts section, updated footer links and industries list for better content organization. Added `FunfactSingle` component for animated counters.

--- src/components/sections/hero/HeroInnerContact.js
+++ src/components/sections/hero/HeroInnerContact.js
@@ -1,6 +1,8 @@
 import React from "react";
 import Contact3 from "@/components/sections/contacts/Contact3";
 import Contact3Mod from "@/components/sections/contacts/Contact3Mod";
+import FunfactSingle from "@/components/shared/funfact/FunfactSingle";
+import FunfactContact from "@/components/shared/funfact/FunfactContact";
 
 const HeroInnerMod = ({ title, text, breadcrums = [] }) => {
 	return (
@@ -16,6 +18,29 @@ const HeroInnerMod = ({ title, text, breadcrums = [] }) => {
                             <div className="tagline mt">
                                 {text}
                             </div>
+						</div>
+						<div
+							className="h9-about-funfact-port-card-mod wow fadeInUp"
+							data-wow-delay=".6s"
+						>
+							<div className="countup-item">
+								<FunfactContact type={3} currentValue={99} symbol={"+"} />
+								<span className="count-text" style={{ color: 'white !important', marginTop: '10px' }}>
+								Inc5000
+							</span>
+							</div>
+							<div className="countup-item">
+								<FunfactContact type={3} currentValue={15} symbol={"+"} />
+								<span className="count-text" style={{ color: 'white !important', marginTop: '10px' }}>
+								Years
+							</span>
+							</div>
+							<div className="countup-item">
+								<FunfactContact type={3} currentValue={18} symbol={"+"} />
+								<span className="count-text" style={{ color: 'white !important' , marginTop: '10px'}}>
+								Awards
+							</span>
+							</div>
 						</div>
 					</div>
 				</div>
