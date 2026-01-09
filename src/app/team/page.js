978278e41856169b2804127fd78833dc84788019
Akash Ramani <akashr@codemechsolutions.com>
2026-01-09 08:38:50 +0530

    remove unwanted team

--- src/app/team/page.js
+++ src/app/team/page.js
@@ -2,10 +2,10 @@ import Footer from "@/components/layout/footer/Footer";
 import Header from "@/components/layout/header/Header";
 import Cta from "@/components/sections/cta/Cta";
 import HeroInner from "@/components/sections/hero/HeroInner";
-import Team1 from "@/components/sections/teams/Team1";
 import BackToTop from "@/components/shared/others/BackToTop";
 import HeaderSpace from "@/components/shared/others/HeaderSpace";
 import ClientWrapper from "@/components/shared/wrappers/ClientWrapper";
+
 export default function Team() {
 	return (
 		<div>
@@ -17,7 +17,6 @@ export default function Team() {
 					<main>
 						<HeaderSpace />
 						<HeroInner title={"Team"} text={"Team"} />
-						<Team1 type={2} />
 						<Cta />
 					</main>
 					<Footer />
