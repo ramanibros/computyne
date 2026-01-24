e44d75d85b350eee2b922f7048feae1bc15c1b2a
GPC Yash <gpc.yashdeveloper@gmail.com>
2026-01-24 12:23:28 +0530

    changes in homen page and navbar

--- src/components/sections/home-page/IndustriesCard.js
+++ src/components/sections/home-page/IndustriesCard.js
@@ -1,7 +1,7 @@
 import Link from "next/link";
 
 const IndustriesCard = ({ industry, idx }) => {
-	const { title, desc, id, img, url} = industry || {};
+	const { title, desc, id, img, url, subData} = industry || {};
 	return (
 		<div className="blog-item style-2">
 			<div className="blog-thumb">
@@ -16,17 +16,25 @@ const IndustriesCard = ({ industry, idx }) => {
 						{title}
 					</h4>
 					<p className="desc  wow fadeInUp" data-wow-delay=".8s">
-						Behind every solution we deliver, there are real people, real expertise, and a commitment to your success.
+						{desc}
 					</p>
 					<div className="desc blog-text wow fadeInUp" data-wow-delay=".8s">
 						<ul className="wow fadeInUp">
-							<li><span><i className="tji-check"></i></span>Personalization at Scale</li>
+							{/* <li><span><i className="tji-check"></i></span>Personalization at Scale</li>
 							<li><span><i className="tji-check"></i></span>Improved Customer Retention</li>
 							<li><span><i className="tji-check"></i></span>Data-Driven Insights</li>
 							<li><span><i className="tji-check"></i></span>Omni-channel Integration</li>
 							<li><span><i className="tji-check"></i></span>Customer Retention</li>
 							<li><span><i className="tji-check"></i></span>Support Optimization</li>
-							<li><span><i className="tji-check"></i></span>Proactive Engagement</li>
+							<li><span><i className="tji-check"></i></span>Proactive Engagement</li> */}
+							{subData.map((item, index) => (
+								<li key={index}>
+									<span>
+										<i className="tji-check"></i>
+									</span>
+									{item}
+								</li>
+							))}
 						</ul>
 					</div>
 
