bfab188e838cba256b94483907e9faae1c5d466b
Akash Ramani <akashr@codemechsolutions.com>
2026-01-25 17:31:38 +0530

    google tag manager added

--- src/app/layout.js
+++ src/app/layout.js
@@ -41,6 +41,25 @@ export const metadata = {
 export default function RootLayout({children}) {
     return (
         <html lang="en" data-scroll-behavior="smooth" dir="ltr">
+
+        <head>
+            {/* Google Tag Manager */}
+            <Script id="gtm-script" strategy="afterInteractive">
+                {`
+            (function(w,d,s,l,i){
+              w[l]=w[l]||[];
+              w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});
+              var f=d.getElementsByTagName(s)[0],
+                  j=d.createElement(s),
+                  dl=l!='dataLayer'?'&l='+l:'';
+              j.async=true;
+              j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
+              f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-KFWT3PN');
+          `}
+            </Script>
+        </head>
+
         <body className={`${bodyFont.variable} ${headingFont.variable}`}>
         {children}
 
